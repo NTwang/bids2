f950f250f762e9f3ca8fcbf5331f4aa07ebcf4fd
NT.Wang <w827764675@sina.com>
2019-08-30 11:09:15 +0800

    Add files via upload

---
+++ static/js/56.a16e53c1.chunk1567132944760.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[56],{1247:function(e,a,n){"use strict";n.r(a);var s=n(24);a.default={namespace:"accountPermissions",state:{menus:[]},reducers:{update:function(e,a){var n=a.payload;return Object(s.a)({},e,n)}},effects:{}}}}]);
